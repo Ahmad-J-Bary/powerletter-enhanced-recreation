c7cb1ed5776dbe00fd31bf2dbd9adc38aa35eb2c
Ahmad-J-Bary <ahmad.j.bary@example.com>
2025-08-03 05:45:54 -0400

    feat: Integrate ThemeProvider and GameModeProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ThemeProvider } from './contexts/ThemeContext.jsx'
+import { GameModeProvider } from './contexts/GameModeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <GameModeProvider>
+        <App />
+      </GameModeProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
